bfb6a9d62747c8eeb580a07a4e49e336056b8d1c
Tyler Wanek <tylerw@axosoft.com>
2016-09-02 15:31:16 -0700

    document utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,6 +3,20 @@ const _ = require('lodash');
 const constants = require('./constants');
 const types = require('./propTypes');
 
+/**
+ * Reduces list or lists type into a single array of rows, as well as collect useful information about rows as they are
+ * collected.
+ * @param  {object} prevState             The previous state of the reduce function. Matches the return value.
+ * @param  {string} headerClassName       className to attach to the row containing the header component of a given list
+ * @param  {renderable} headerComponent   a React renderable component that will be the header of a given list
+ * @param  {object} headerProps           props to pass to the header component
+ * @param  {number} height                The height of the header component
+ * @param  {boolean} initCollapsed        whether a given list is collapsed or not. This only applies when constructing
+ *                                        the first row state
+ * @param  {array} rows                   List of rows to copy into state
+ * @param  {number} index                 index of the reduce operation
+ * @return {object}                       see comment at the return
+ */
 function reduceRowsIntoRowConfig(
   prevState, { headerClassName, headerComponent, headerProps, height, initCollapsed, rows: inRows }, index
 ) {
@@ -64,18 +78,26 @@ function reduceRowsIntoRowConfig(
   }
 
   return {
-    adjustHeaderOffset: newHeaderOffset,
-    collapsedSections,
-    contentHeight,
-    headers,
-    stackHeaders,
-    offsetBuffer,
-    offsetCount: nextOffset,
-    partitions,
-    rows: outRows
+    adjustHeaderOffset: newHeaderOffset, // keeps a running tally of the headeroffsets, used for locking/stagnig headers
+    collapsedSections, // an array of boolean values corresponding to each section in the lists object
+    contentHeight, // the height of the content contained by this list
+    headers, // an array containing header data, (offsets, row index, height, lockPosition)
+    stackHeaders, // should we be calculating headerOffsets from height
+    offsetBuffer, // number of rows to display per partition
+    offsetCount: nextOffset, // keeps track of partition size inbetween reductions
+    partitions, // an array mapping the starting partition indices to starting vertical transforms
+    rows: outRows // the rows that we will use as state
   };
 }
 
+/**
+ * Initializes the reduce call to reduceRowsIntoRowConfig. The step where we convert a list prop into lists is done here
+ * @param  {array} list                     List or lists prop.
+ * @param  {number} offsetBuffer            The number of rows per partition
+ * @param  {boolean} stackHeaders           Signifies that we should do the math to calculate stacking header offsets
+ * @param  {Array}  [collapsedSections=[]]  The collapsedSection state from the scrollable class
+ * @return {object}                         Reduces the list/lists into consumable state for the scrollable
+ */
 function buildRowConfig(list, offsetBuffer, stackHeaders, collapsedSections = []) {
   const offsetCount = offsetBuffer - 1;
 
@@ -118,16 +140,39 @@ function buildRowConfig(list, offsetBuffer, stackHeaders, collapsedSections = []
   }));
 }
 
+/**
+ * assign meaning to math
+ * @param  {number} contentHeight the height of the content
+ * @param  {number} offsetHeight  the offsetHeight from the vertical scrollbar
+ * @return {number}               the difference between contentHeight and offsetHeight
+ */
 function getMaxHeight(contentHeight, offsetHeight) {
   return contentHeight - offsetHeight;
 }
 
+/**
+ * calculates the width of a gutter via mouse movement. Also takes into account minWidth bound of a gutter.
+ * @param  {string} side             which side are we resizing
+ * @param  {Number} [minWidth=0]     the minimum value the gutter may be resized to
+ * @param  {number} baseWidth        the width we started with when we began resizing
+ * @param  {number} startingPosition position our mouse was in when we started resizing
+ * @param  {number} currentPosition  current position of our mouse
+ * @return {number}                  the next width of the gutter
+ */
 function getResizeWidth(side, minWidth = 0, baseWidth, startingPosition, currentPosition) {
   const deltaWidth = startingPosition - currentPosition;
   const modifier = side === constants.handleClass.LEFT ? -1 : 1;
   return Math.max(minWidth, baseWidth + (modifier * deltaWidth));
 }
 
+/**
+ * calculates which partition is the top of our renderable content and clamps verticalTransform to within min/max values
+ * @param  {number} preClampTransform vertical transform before we clamp it between min / max values
+ * @param  {number} maxHeight         maximum height our vertical transform can reach
+ * @param  {array} partitions         list of numbers which represent the starting vertical transformation of partitions
+ *                                    by index (partitions[5] is the starting vertical transform of the 6th partition)
+ * @return {object}                   topPartionIndex and the clamped verticalTransform
+ */
 function getScrollValues(preClampTransform, maxHeight, partitions) {
   const verticalTransform = _.clamp(preClampTransform, 0, maxHeight);
   const topPartitionIndex = _.findIndex(
@@ -136,6 +181,11 @@ function getScrollValues(preClampTransform, maxHeight, partitions) {
   return { topPartitionIndex, verticalTransform };
 }
 
+/**
+ * helper function that returns a very strict width object for gutters
+ * @param  {Number} [width=0] the width we wish to express as a style object
+ * @return {object}           a style object representing a the given width
+ */
 function getWidthStyle(width = 0) {
   return _.isNumber(width) && width > 0 ? {
     minWidth: `${width}px`,
